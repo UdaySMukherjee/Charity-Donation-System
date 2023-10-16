e3da60366f2dcd71bd43a5fe36816af28542a843
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2023-10-16 09:29:45 +0530

    Delete Charity directory

--- Charity/signup/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
-<script src="../bootstrap/js/jquery.min.js"></script>
-<script src="../bootstrap/js/popper.min.js"></script>
-<script src="../bootstrap/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="../bootstrap/css/style.css">
-<link rel="shortcut icon" href="../images/index/logo.png" type="image/x-icon">
